c9f67cb99cba6c52cf58d6d927511ec3f6c08f8e
Constantin Câmpean <constantin.campean@axsys.ro>
2020-09-14 11:39:37 +0300

    fix the bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const past = require("./past")
 const future = require("./future")
 
 app.get("/:age", (req, res) => {
-  res.send(past(req.params.age, 10) + future(req.params.future, 10))
+  res.send(past(req.params.age, 10) + future(req.params.age, 10))
 })
 
 app.listen(5000)
