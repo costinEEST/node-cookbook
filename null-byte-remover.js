85a715d27da49a08eb25e6d13cc0ba9432be2032
Constantin Câmpean <constantin.campean@axsys.ro>
2020-09-28 21:21:24 +0300

    asyncronous file operations

--- null-byte-remover.js
+++ null-byte-remover.js
@@ -1,14 +1,23 @@
+setInterval(() => process.stdout.write("."), 10).unref()
+
 const fs = require("fs")
 const path = require("path")
-
 const cwd = process.cwd()
-const bytes = fs.readFileSync(path.join(cwd, "file.dat"))
 
-const clean = bytes.filter((n) => n)
-
-fs.writeFileSync(path.join(cwd, "clean.dat"), clean)
-
-fs.appendFileSync(
-  path.join(cwd, "log.txt"),
-  new Date() + " " + (bytes.length - clean.length) + " bytes removed\n"
-)
+fs.readFile(path.join(cwd, "file.dat"), (err, bytes) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  const clean = bytes.filter((n) => n)
+  fs.writeFile(path.join(cwd, "clean.dat"), clean, (err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    fs.appendFile(
+      path.join(cwd, "log.txt"),
+      new Date() + " " + (bytes.length - clean.length) + " bytes removed\n"
+    )
+  })
+})
